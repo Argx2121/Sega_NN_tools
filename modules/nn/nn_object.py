749621fc631a2a64b0a3c792fb7bbdde3f237c7f
Argg <sleepydersedreamer@gmail.com>
2020-10-25 15:56:26 +1100

    restructured to reduce statements split over lines

--- modules/nn/nn_object.py
+++ modules/nn/nn_object.py
@@ -11,9 +11,18 @@ class Read:
         self.format_type = format_type
         self.debug = debug
 
-    def _execute(self, seek, text, function):
-        self.f.seek(seek)
-        return console_out(text, function)
+    def _execute(self, seek, text_index: int, function):
+        text_list = ["Parsing Model Info...", "Parsing Bones...", "Parsing Materials...", "Parsing Faces...",
+                     "Parsing Vertices..."]
+        self.f.seek(self.post_info + seek)
+        return console_out(text_list[text_index], function)
+
+    def _start(self):
+        f = self.f
+        start_block = f.tell() - 4
+        len_block = read_int(f)
+        stdout.write("| \n")
+        return start_block, len_block
 
     @dataclass
     class ModelData:
@@ -26,68 +35,60 @@ class Read:
         build_mesh: Any = None
 
     def type_1(self) -> ModelData:
+        start_block, len_block = self._start()
         f = self.f
-        start_block = f.tell() - 4
-        len_block = read_int(f)
         model = self.ModelData()
-        stdout.write("| \n")
+        post = self.post_info
 
-        data, self.post_info = self._execute(
-            read_int(f) + self.post_info, "Parsing Model Info...", model_data.Read(f, self.post_info, start_block).xbox)
+        data, self.post_info = self._execute(read_int(f) + post, 0, model_data.Read(f, post, start_block).xbox)
         model.data = data
-        post_info = self.post_info
+        post = self.post_info
         if self.debug:
             print(data)
-            print("After info offset (decimal, memory rips / files with broken pointers will be negative):", post_info)
+            print("After info offset (decimal, memory rips / files with broken pointers will be negative):", post)
 
-        model.bones = self._execute(
-            post_info + data.bone_offset, "Parsing Bones...", bones.Read(f, data.bone_count).type_1)
-        model.materials = self._execute(post_info + data.material_offset, "Parsing Materials...",
-                                        materials.Read(f, post_info, data.material_count).type_1)
-        model.faces = self._execute(
-            post_info + data.face_set_offset, "Parsing Faces...", faces.Read(f, post_info, data.face_set_count).xbox)
+        model.bones = self._execute(data.bone_offset, 1, bones.Read(f, data.bone_count).type_1)
+
+        model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).type_1)
+
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).xbox)
+
         model.vertices, model.mesh_info = self._execute(
-            post_info + data.vertex_buffer_offset, "Parsing Vertices...",
-            vertices.Read(f, post_info, self.format_type, data.vertex_buffer_count).xbox)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).xbox)
 
-        model.build_mesh = \
-            console_out("Parsing Sub Mesh Data...", meshes.Read(
-                f, post_info, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).xbox)
-        # seeks in method
+        model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).xbox)  # seeks in method
+
         if self.debug:
             print(model.build_mesh)
         f.seek(start_block + len_block + 8)  # seek end of block
         return model
 
     def type_2(self) -> ModelData:
+        start_block, len_block = self._start()
         f = self.f
-        start_block = f.tell() - 4
-        len_block = read_int(f)
         model = self.ModelData()
-        stdout.write("| \n")
+        post = self.post_info
 
-        data, self.post_info = self._execute(
-            read_int(f) + self.post_info, "Parsing Model Info...", model_data.Read(f, self.post_info, start_block).xbox)
+        data, self.post_info = self._execute(read_int(f) + post, 0, model_data.Read(f, post, start_block).xbox)
         model.data = data
-        post_info = self.post_info
+        post = self.post_info
         if self.debug:
             print(data)
-            print("After info offset (decimal, memory rips / files with broken pointers will be negative):", post_info)
+            print("After info offset (decimal, memory rips / files with broken pointers will be negative):", post)
 
-        model.bones = self._execute(
-            post_info + data.bone_offset, "Parsing Bones...", bones.Read(f, data.bone_count).type_1)
-        model.materials = self._execute(post_info + data.material_offset, "Parsing Materials...",
-                                        materials.Read(f, post_info, data.material_count).type_2)
-        model.faces = self._execute(
-            post_info + data.face_set_offset, "Parsing Faces...", faces.Read(f, post_info, data.face_set_count).xbox)
+        model.bones = self._execute(data.bone_offset, 1, bones.Read(f, data.bone_count).type_1)
+
+        model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).type_2)
+
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).xbox)
+
         model.vertices, model.mesh_info = self._execute(
-            post_info + data.vertex_buffer_offset, "Parsing Vertices...",
-            vertices.Read(f, post_info, self.format_type, data.vertex_buffer_count).xbox)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).xbox)
 
-        model.build_mesh = \
-            console_out("Parsing Sub Mesh Data...", meshes.Read(
-                f, post_info, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).xbox)
-        # seeks in method
+        model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).xbox)  # seeks in method
+
         if self.debug:
             print(model.build_mesh)
         f.seek(start_block + len_block + 8)  # seek end of block
