65d179851e05308c3ab480131584a2ee26842d2a
Argg <sleepydersedreamer@gmail.com>
2020-11-07 02:07:20 +1100

    function renames, added eno support

--- modules/nn/nn_object.py
+++ modules/nn/nn_object.py
@@ -1,5 +1,5 @@
 from .object_assets import model_data, materials, faces, vertices, bones, meshes
-from .object_assets.nn_util import implicit_faces
+from .object_assets.obj_util import implicit_faces_fix_mesh_size
 from ..util import *
 from dataclasses import dataclass
 
@@ -52,13 +52,13 @@ class Read:
 
         model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).xno)
 
-        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).type_1)
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).le_1)
 
         model.vertices, model.mesh_info = self._execute(
-            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).type_1)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).le_1)
 
         model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
-            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).type_1)  # seeks in method
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).le_1)  # seeks in method
 
         if self.debug:
             print(model.build_mesh)
@@ -82,13 +82,13 @@ class Read:
 
         model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).zno)
 
-        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).type_1)
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).le_1)
 
         model.vertices, model.mesh_info = self._execute(
-            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).type_1)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).le_1)
 
         model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
-            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).type_1)  # seeks in method
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).le_1)  # seeks in method
 
         if self.debug:
             print(model.build_mesh)
@@ -112,14 +112,14 @@ class Read:
 
         model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).lno)
 
-        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).type_2)
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).le_2)
 
         model.vertices, model.mesh_info = self._execute(
-            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).type_2)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).le_2)
 
         print(data.mesh_data_offset)
         model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
-            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).type_2)  # seeks in method
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).le_2)  # seeks in method
 
         if self.debug:
             print(model.build_mesh)
@@ -144,16 +144,45 @@ class Read:
         model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).sno)
 
         model.vertices, model.mesh_info = self._execute(
-            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).type_3)
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).le_3)
 
         var = console_out_pre("Generating faces")
-        model.faces, model.vertices = implicit_faces(model.vertices)
+        model.faces, model.vertices = implicit_faces_fix_mesh_size(model.vertices)
         console_out_post(var)
 
         model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
-            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).type_3)  # seeks in method
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).le_3)  # seeks in method
 
         if self.debug:
             print(model.build_mesh)
         f.seek(start_block + len_block + 8)  # seek end of block
+        return model
+
+    def eno(self) -> ModelData:
+        start_block, len_block = self._start()  # len block is wrong btw
+        f = self.f
+        model = self.ModelData()
+        post = self.post_info
+
+        data, self.post_info = self._execute(read_int(f, ">"), 0, model_data.Read(f, post, start_block).be_full)
+        model.data = data
+        post = self.post_info
+        if self.debug:
+            print(data)
+            print("After info offset (decimal, memory rips / files with broken pointers will be negative):", post)
+
+        model.bones = self._execute(data.bone_offset, 1, bones.Read(f, data.bone_count).be_full)
+
+        model.materials = self._execute(data.material_offset, 2, materials.Read(f, post, data.material_count).eno)
+
+        model.faces = self._execute(data.face_set_offset, 3, faces.Read(f, post, data.face_set_count).be_1)
+
+        model.build_mesh = console_out("Parsing Sub Mesh Data...", meshes.Read(
+            f, post, data.mesh_sets_count, data.mesh_data_offset, data.mesh_data_count).be_1)  # seeks in method
+
+        model.vertices, model.mesh_info = self._execute(
+            data.vertex_buffer_offset, 4, vertices.Read(f, post, self.format_type, data.vertex_buffer_count).be_1)
+
+        if self.debug:
+            print(model.build_mesh)
         return model
