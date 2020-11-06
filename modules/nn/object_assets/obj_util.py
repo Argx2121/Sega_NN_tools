764c25c8f1796992ecd2dbfd55b2faa128158f88
Argg <sleepydersedreamer@gmail.com>
2020-11-07 02:06:18 +1100

    renamed function and changed

--- modules/nn/object_assets/obj_util.py
+++ modules/nn/object_assets/obj_util.py
@@ -20,7 +20,7 @@ class FaceList:
     faces: list
 
 
-def implicit_faces(vertex_info):
+def implicit_faces_fix_mesh_size(vertex_info):
     # we generate faces then fix both sets
     face_mesh = []
     for data in vertex_info:  # sub mesh
@@ -28,7 +28,8 @@ def implicit_faces(vertex_info):
         count = 0
         for info in data:  # sub sub mesh, some of these are 3 vertices long
             face_count = len(info.positions) - 2
-            net_count = count + face_count
+            if face_count < 0:
+                face_count = 0
 
             for loop in range(face_count // 2):
                 loop *= 2
@@ -37,6 +38,7 @@ def implicit_faces(vertex_info):
                 face_mesh_list.append((loop + 2, loop + 1, loop + 3))
 
             if face_count % 2 != 0:
+                net_count = count + face_count
                 face_mesh_list.append((net_count - 1, net_count, net_count + 1))
             count += len(info.positions)
 
@@ -55,4 +57,7 @@ def implicit_faces(vertex_info):
             list(chain.from_iterable([info.normals2 for info in data])),
             list(chain.from_iterable([info.normals3 for info in data])),
         ))  # flat list
+    vertex_info = None
+    import gc
+    gc.collect()
     return face_mesh, vertex_list
