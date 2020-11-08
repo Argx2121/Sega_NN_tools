d79eb8e4a0ef0d09b497221b39de9325df4aa99e
Argg <sleepydersedreamer@gmail.com>
2020-11-08 13:29:02 +1100

    clean up

--- modules/nn/object_assets/vertices.py
+++ modules/nn/object_assets/vertices.py
@@ -217,21 +217,6 @@ class Read:
                     return
                 v_cols_list.append([cols[2] / div_by, cols[1] / div_by, cols[0] / div_by, cols[3] / div_by])
 
-            # 000KC0NP 0WWW0000 000000QU 00000000  KC0NW0P0 000000QU 00000000 00000000 SR PC
-            #  K = colours as shorts, not bytes Q = unknown, in psu Q is wx but srpc has only one float
-            #  block order: POS Q WEIGH NORM COL UV SR PC
-            # 0M00C0NP 0WWW0I02 0000000U 00000000  0C0NW0P0 000W000U 00000000 00000000 06
-            #  M = 2 extra normal sets, I = bone indices
-            # 00000001 00000000 00000011 00000000 00000010 00000011 00000000 00000000 s4e1
-            # p, uw,
-
-            # latest goes first and should be of the latest games format
-            def latest_x():
-                sonic_2006_x()
-
-            def latest_z():
-                sonic_4_episode_1_z()
-
             def sonic_riders_x():
                 for _ in range(vertex_count):
                     pos(BitFlags.position)
@@ -271,35 +256,30 @@ class Read:
                         f.seek(16, 1)
                     extra_norms(BitFlags.extra_normals)
 
-            dict_x = {
+            format_dict = {
                 "SonicRiders_X": sonic_riders_x, "PhantasyStarUniverse_X": phantasy_star_universe_x,
-                "Sonic2006_X": sonic_2006_x}
-            dict_z = {
-                "Sonic4Episode1_Z": sonic_4_episode_1_z}
-            format_type = self.format_type
-            if format_type[-1] == "X":
-                if format_type in dict_x:
-                    dict_x[format_type]()
-                else:
-                    latest_x()
-            elif format_type[-1] == "Z":
-                if format_type in dict_z:
-                    dict_z[format_type]()
-                else:
-                    latest_z()
+                "Sonic2006_X": sonic_2006_x,
+                "Sonic4Episode1_Z": sonic_4_episode_1_z,
+            }
+            format_dict[self.format_type]()
 
         for var in range(self.vertex_buffer_count):  # for all sub meshes
             v_pos_list, v_norms_list, v_uvs_list, v_wxs_list, v_b_wei_list, v_cols_list = [], [], [], [], [], []
             v_b_index_list, v_norms2_list, v_norms3_list = [], [], []
-            # parse vertices
             f.seek(self.vertex_mesh_offset[var] + self.post_nxif)
-            # go through each sub meshes block size
             vertex_count = self.mesh_info[var].vertex_count
             block_type = self.mesh_info[var].vertex_block_type
             from enum import Flag
 
             class BitFlags(Flag):
-                # generic
+                # 000KC0NP 0WWW0000 000000QU 00000000  KC0NW0P0 000000QU 00000000 00000000 SR PC
+                #  K = colours as shorts, not bytes Q = unknown, in psu Q is wx but srpc has only one float
+                #  block order: POS Q WEIGH NORM COL UV SR PC
+                # 0M00C0NP 0WWW0I02 0000000U 00000000  0C0NW0P0 000W000U 00000000 00000000 06
+                #  M = 2 extra normal sets, I = bone indices
+                # 00000001 00000000 00000011 00000000 00000010 00000011 00000000 00000000 s4e1
+                # p, uw,
+
                 uv = block_type >> 16 & 1
                 position = block_type >> 25 & 1
                 normal = block_type >> 28 & 1
@@ -309,7 +289,6 @@ class Read:
                 weight_indices = block_type >> 50 & 1
                 wx = block_type >> 17 & 1
                 extra_normals = block_type >> 48 & 1
-                # keep generic values here
 
             vert_block()
             vertex_data.append(
@@ -343,15 +322,6 @@ class Read:
                     else:
                         v_b_wei_list.append((w1, w2, w3, 1 - w1 - w2 - w3))
 
-            # 00000000 00PP000P 0WWW0X80 80000P0P 00000000 00000000 000X000P 000PW0P0 sonic free riders
-            # 00000000 00110010 01110110 10000101 00000000 00000000 00010010 00011010 ?? size 48
-            # I didn't have enough samples to check if P is P or UV but made a guess for 1 based on similar structures
-            # order Pos Wei indeX Uv 8 (no idea but skip 8)
-
-            # latest goes first and should be of the latest games format
-            def latest_e():
-                sonic_free_riders_e()
-
             def sonic_free_riders_e():
                 for _ in range(vertex_count):
                     pos(BitFlags.position)
@@ -362,14 +332,10 @@ class Read:
                     if BitFlags.unk2:
                         f.seek(4, 1)
 
-            dict_e = {
-                "SonicFreeRiders_E": sonic_free_riders_e}
-            format_type = self.format_type
-            if format_type[-1] == "E":
-                if format_type in dict_e:
-                    dict_e[format_type]()
-                else:
-                    latest_e()
+            format_dict = {
+                "SonicFreeRiders_E": sonic_free_riders_e,
+            }
+            format_dict[self.format_type]()
 
         for var in range(self.vertex_buffer_count):  # for all sub meshes
             v_pos_list, v_norms_list, v_uvs_list, v_wxs_list, v_b_wei_list, v_cols_list = [], [], [], [], [], []
@@ -380,14 +346,17 @@ class Read:
             from enum import Flag
 
             class BitFlags(Flag):
-                # generic
+                # 00000000 00PP000P 0WWW0X80 80000P0P 00000000 00000000 000X000P 000PW0P0 sonic free riders
+                # 00000000 00110010 01110110 10000101 00000000 00000000 00010010 00011010 ?? size 48
+                # I didn't have enough samples to check if P is P or UV but guessed for 1 based on similar structures
+                # order Pos Wei indeX Uv 8 (no idea but skip 8)
+
                 position = block_type >> 1 & 1
                 weights = block_type >> 3 & 1
                 index = block_type >> 12 & 1
                 uv = block_type >> 4 & 1
                 unk1 = block_type >> 39 & 1
                 unk2 = block_type >> 9 & 1
-                # keep generic values here
 
             vert_block()
             vertex_data.append(
@@ -430,18 +399,6 @@ class Read:
                     else:
                         v_b_wei_list.append((w1, w2, w3, 1 - w1 - w2 - w3))
 
-            # 00000000 000?00XU 0000IW0? 0?0??0N/P house of the dead 4
-            # either have U or X on - you shouldn't have both
-            # order:
-            # Pos Weights boneIndices ? Uvs wX ?
-            # I only had samples with both W and I, so it may be WI instead of IW
-            # I only had samples with all unknowns active - all unknowns have been assigned to the first ?
-            # / is an unknown as well but I could actually identify the section size
-
-            # latest goes first and should be of the latest games format
-            def latest_c():
-                house_of_the_dead_4_c()
-
             def house_of_the_dead_4_c():
                 for _ in range(vertex_count):
                     pos(BitFlags.position)
@@ -454,14 +411,10 @@ class Read:
                     if BitFlags.unknown2:
                         f.seek(12, 1)
 
-            dict_c = {
-                "HouseOfTheDead4_C": house_of_the_dead_4_c}
-            format_type = self.format_type
-            if format_type[-1] == "C":
-                if format_type in dict_c:
-                    dict_c[format_type]()
-                else:
-                    latest_c()
+            format_dict = {
+                "HouseOfTheDead4_C": house_of_the_dead_4_c,
+            }
+            format_dict[self.format_type]()
 
         for var in range(self.vertex_buffer_count):  # for all sub meshes
             v_pos_list, v_norms_list, v_uvs_list, v_wxs_list, v_b_wei_list, v_cols_list = [], [], [], [], [], []
@@ -472,7 +425,14 @@ class Read:
             from enum import Flag
 
             class BitFlags(Flag):
-                # generic
+                # 00000000 000?00XU 0000IW0? 0?0??0N/P house of the dead 4
+                # either have U or X on - you shouldn't have both
+                # order:
+                # Pos Weights boneIndices ? Uvs wX ?
+                # I only had samples with both W and I, so it may be WI instead of IW
+                # I only had samples with all unknowns active - all unknowns have been assigned to the first ?
+                # / is an unknown as well but I could actually identify the section size
+
                 position = block_type & 1
                 unknown2 = block_type >> 1 & 1
                 uv = block_type >> 16 & 1
