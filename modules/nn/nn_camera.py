66819f3de8288fd06f8594623e9f9ef0cda70677
Argx2121 <sleepydersedereamer@gmail.com>
2021-11-10 21:58:30 +1100

    updated N*CA block reading (Camera info)

--- modules/nn/nn_camera.py
+++ modules/nn/nn_camera.py
@@ -1,4 +1,16 @@
 from ..util import *
+from dataclasses import dataclass
+
+
+@dataclass
+class Camera:
+    fov: float
+    roll: float
+    # ratio: float  # not usable - can't set on a per camera basis
+    clip_near: float
+    clip_far: float
+    position: Tuple[float, ...]
+    target: Tuple[float, ...]
 
 
 class Read:
@@ -7,9 +19,9 @@ class Read:
     def __init__(self, f: BinaryIO):
         """Reads a N*CA block.
 
-        Usage : Optional
+        Usage : Required
 
-        Function : Storing camera data (not applicable to blender)
+        Function : Storing camera data
 
 
         Parameters
@@ -21,4 +33,28 @@ class Read:
 
     def type_1(self):
         f = self.f
-        f.seek(read_int(f), 1)
+        start_block = f.tell() - 4
+        end_of_block = start_block + read_int(f) + 8
+        f.seek(start_block + 20)
+        fov = unpack("h", f.read(2))[0] * (360 / 32767)
+        roll = unpack("h", f.read(2))[0] * (360 / 32767)
+        _ = read_float(f)  # ratio
+        clip_near = read_float(f)
+        clip_far = read_float(f)
+        cam = Camera(fov, roll, clip_near, clip_far, read_float_tuple(f, 3), read_float_tuple(f, 3))
+        f.seek(end_of_block)
+        return cam
+
+    def type_2(self):
+        f = self.f
+        start_block = f.tell() - 4
+        end_of_block = start_block + read_int(f) + 8
+        f.seek(start_block + 20)
+        roll = unpack(">h", f.read(2))[0] * (360 / 32767)
+        fov = unpack(">h", f.read(2))[0] * (360 / 32767)
+        _ = read_float(f, ">")  # ratio
+        clip_near = read_float(f, ">")
+        clip_far = read_float(f, ">")
+        cam = Camera(fov, roll, clip_near, clip_far, read_float_tuple(f, 3, ">"), read_float_tuple(f, 3, ">"))
+        f.seek(end_of_block)
+        return cam
