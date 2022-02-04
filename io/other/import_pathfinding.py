1c7f9811c05f6330bf0b66e8a8d301aab2c13113
Argx2121 <sleepydersedereamer@gmail.com>
2022-02-04 18:51:13 +1100

    SRGC pathfinding importing support

--- io/other/import_pathfinding.py
+++ io/other/import_pathfinding.py
@@ -4,6 +4,8 @@ from bpy_extras.io_utils import ImportHelper
 
 from Sega_NN_tools.io.import_util import batch_handler
 from Sega_NN_tools.modules.util import print_line
+
+from Sega_NN_tools.modules.other.pathfinding.srgc import ReadGc as Srgc
 from Sega_NN_tools.modules.other.pathfinding.srpc import ReadXbox as Srpc
 from Sega_NN_tools.modules.other.pathfinding.srzg import ReadWii as Srzg
 from Sega_NN_tools.modules.other.pathfinding.sfr import ReadX360 as Sfr
@@ -25,7 +27,8 @@ class ImportSegaNNPathfinding(bpy.types.Operator, ImportHelper):
         description="Game variant",
         items=(
             ("Match__", "Match", "Tries to match the file with a known format (Experimental!!)"),
-            ("SonicRiders_X", "Sonic Riders", "Sonic Riders Pathfinding | 21 Feb 2006"),
+            ("SonicRiders_G", "Sonic Riders GC", "Sonic Riders Gamecube Pathfinding| 21 Feb 2006"),
+            ("SonicRiders_X", "Sonic Riders PC", "Sonic Riders Pc Pathfinding | 21 Feb 2006"),
             ("SonicRidersZeroGravity_G", "Sonic Riders Zero Gravity",
              "Sonic Riders Zero Gravity Pathfinding | 8 Jan 2008"),
             ("SonicFreeRiders_E", "Sonic Free Riders", "Sonic Free Riders Pathfinding | 4 Nov 2010"),
@@ -55,7 +58,8 @@ class ImportSegaNNPathfinding(bpy.types.Operator, ImportHelper):
 
 
 def pathfinding_import(filepath, game, settings):
-    game_dict = {"SonicRiders_X": Srpc, "SonicRidersZeroGravity_G": Srzg, "SonicFreeRiders_E": Sfr}
+    game_dict = {
+        "SonicRiders_G": Srgc, "SonicRiders_X": Srpc, "SonicRidersZeroGravity_G": Srzg, "SonicFreeRiders_E": Sfr}
 
     def execute_set(file_path):
         f = open(file_path, 'rb')
---
+++ modules/other/pathfinding/srgc.py
@@ -0,0 +1,37 @@
+from Sega_NN_tools.modules.util import *
+
+
+class ReadGc:
+    def __init__(self, f):
+        self.f = f
+        self.position = []
+        self.edge = []
+
+    def execute(self):
+        console_out("Reading Pathfinding...", self.read_data)
+        console_out("Making Pathfinding...", self.make_blender)
+
+    def read_data(self):
+        f = self.f
+        start = f.tell()
+        f.seek(8, 1)
+        to_verts, to_edges = read_int_tuple(f, 2, ">")
+        f.seek(12, 1)
+        vert_count, edge_count = read_int_tuple(f, 2, ">")
+
+        f.seek(start + to_verts)
+        for i in range(vert_count):
+            self.position.append(read_float_tuple(f, 3, ">"))
+
+        f.seek(start + to_edges)
+        for _ in range(edge_count):
+            self.edge.append(read_short_tuple(f, 2, ">"))
+            f.seek(44, 1)
+
+    def make_blender(self):
+        mesh = bpy.data.meshes.new("Pathfinding")
+        mesh.from_pydata(self.position, self.edge, [])
+        obj = bpy.data.objects.new(mesh.name, mesh)
+        obj.rotation_euler[0] = 1.5708
+
+        bpy.context.collection.objects.link(obj)
