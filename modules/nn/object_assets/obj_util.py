44447224a18df304e28620a8bd5f96bf8344137b
Argx2121 <argx2121@gmail.com>
2026-02-08 15:05:09 +1030

    fix for snos triangle stripping "technique"

--- modules/nn/object_assets/obj_util.py
+++ modules/nn/object_assets/obj_util.py
@@ -11,14 +11,40 @@ def implicit_faces_fix_mesh_size(vertex_info):
         count = 0
         for info in data:  # sub sub mesh, some of these are 3 vertices long
             face_count = len(info.positions) - 2
+            test_list = []
+            compiled_vert_data = []
+            if info.positions:
+                test_list.append(info.positions)
+            if info.weights and info.weights[0] != []:
+                test_list.append(info.weights)
+            if info.bone_list_indices and info.bone_list_indices[0] != []:
+                test_list.append(info.bone_list_indices)
+            if info.normals and info.normals[0] != []:
+                test_list.append(info.normals)
+            if info.uvs and info.uvs[0] != []:
+                test_list.append(info.uvs)
+            if info.wxs and info.wxs[0] != []:
+                test_list.append(info.wxs)
+            if info.colours and info.colours[0] != []:
+                test_list.append(info.colours)
+
+            for the_stuff in zip(*test_list):
+                compiled_vert_data.append(the_stuff)
+
             if face_count < 0:
                 face_count = 0
 
             for loop in range(face_count // 2):
                 loop *= 2
-                loop += count
-                face_mesh_list.append((loop, loop + 1, loop + 2))
-                face_mesh_list.append((loop + 2, loop + 1, loop + 3))
+                loop1 = (loop, loop + 1, loop + 2)
+                loop2 = (loop + 2, loop + 1, loop + 3)
+
+                face_test_again = [compiled_vert_data[a] for a in loop1]
+                if len(set(face_test_again)) == 3:
+                    face_mesh_list.append([a + count for a in loop1])
+                face_test_again = [compiled_vert_data[a] for a in loop2]
+                if len(set(face_test_again)) == 3:
+                    face_mesh_list.append([a + count for a in loop2])
 
             if face_count % 2 != 0:
                 net_count = count + face_count
